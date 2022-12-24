058d1a72ec957a059f30a214c756dd621e8a0f12
Songchen Tan <i@tansongchen.com>
2022-12-24 10:25:19 -0500

    Switch upload provider to AWS

--- config/plugins.js
+++ config/plugins.js
@@ -16,5 +16,23 @@ module.exports = ({ env }) => ({
         customReplacements: Array.from(gb).map(c => [c, pinyin.convertToPinyin(c)]),
       }
     }
-  }
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
